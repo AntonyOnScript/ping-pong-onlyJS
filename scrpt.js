f7eb92de5ba1eeb711c766fe9457561bfa59ee17
Antony <62514607+AntonyOnScript@users.noreply.github.com>
2021-03-16 12:59:41 -0300

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -8,7 +8,7 @@ var points=0
 
 function random(min, max) {
     var num = Math.floor(Math.random() * (max - min + 1)) + min
-    if(num!==0{
+    if(num!==0){
     return num
     }else{
     return random(min,max)
